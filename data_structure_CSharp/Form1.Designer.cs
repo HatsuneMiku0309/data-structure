ad532c5641f24626200747d2097be78bcf559927
HatsuneMiku <tisshuhime@gmail.com>
2017-01-18 10:53:00 +0800

    array process

--- data_structure_CSharp/Form1.Designer.cs
+++ data_structure_CSharp/Form1.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "sun";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "array_insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(79, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(797, 267);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "index";
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "value";
+            this.textBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(210, 357);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "array_insert";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(983, 498);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- data_structure_CSharp/Form1.cs
+++ data_structure_CSharp/Form1.cs
@@ -16,5 +16,67 @@ namespace data_structure_CSharp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] A = new int[] { 50, 60, 70, 80, 90, 100 };
+            int sun = 0;
+            float average = 0;
+            // int? sun = null;
+            
+            for (int i = 0; i < (A.Length-1); i++)
+            {
+                sun += (int)A[i];
+            }
+
+            average = sun / A.Length;
+
+            richTextBox1.AppendText("your sun :" + sun.ToString() + "  and average : " + average.ToString() + Environment.NewLine );
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] A = new int[] { 10, 20, 30 };
+            int index, value = 0;
+            int count = 1;
+            string resultStr = "";
+
+            if (!int.TryParse(textBox1.Text,out index) || !int.TryParse(textBox2.Text, out value))
+            {
+                richTextBox1.AppendText("your input is error" + Environment.NewLine);
+                return;
+            }
+
+            if (index < 0 || index > A.Length-1)
+            {
+                richTextBox1.AppendText("your index is overflow" + Environment.NewLine);
+                return;
+            }
+
+            for (int i = A.Length-1; i > index; i--)
+            {
+                A[i] = A[i - 1];
+            }
+
+            A[index] = value;
+            
+            foreach (var myValue in A) {
+                resultStr += "{" + count + ": " + myValue + "}";
+                ++count;
+            }
+
+            richTextBox1.AppendText(resultStr + Environment.NewLine);
+
+        }
+
+        private void textBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+        }
     }
 }
